a059e930d9f6464d658405395edef96eab847c04
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2025-08-25 21:50:13 +0300

    tweak(core): change url for github repo

--- lib/core/utils/url_launcher.dart
+++ lib/core/utils/url_launcher.dart
@@ -2,7 +2,9 @@ import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncher {
   static Future<void> launchGithubRepository() async {
-    final Uri url = Uri.parse('https://github.com/FussuChalice');
+    final Uri url = Uri.parse(
+      'https://github.com/FussuChalice/programming_calculator',
+    );
 
     if (!await launchUrl(url)) {
       throw Exception('Could not launch github repository for this project');
